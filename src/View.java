6a7f0f502549d5cc799ffb87061d7adfd71aef53
Kenta11 <kenta@unknown>
2017-05-17 13:31:29 +0900

    ver1.1

--- src/View.java
+++ src/View.java
@@ -4,12 +4,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event;
 
-public class View implements MouseListener, MouseMotionListener {
+public class View implements MouseListener{
   private JFrame frame;
   private JPanel board;
   private JButton button[][];
+  private int clicked[2] = new int[2];
+  clicked[0] = 3;
+  clicked[1] = 3;
 
   private ImageIcon nocoin = new ImageIcon("./res/nocoin.png");
   private ImageIcon black  = new ImageIcon("./res/black.png");
@@ -19,7 +25,8 @@ public class View implements MouseListener, MouseMotionListener {
   public View(){
     frame = new JFrame();
     frame.setTitle("Reversi");
-    frame.setSize(500, 500);
+    frame.getContentPane().setPreferredSize(new Dimension(400, 400));
+    frame.pack();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     Container p = frame.getContentPane();
@@ -28,7 +35,7 @@ public class View implements MouseListener, MouseMotionListener {
     button = new JButton[8][8];
     for(int y = 0; y < 8; y++){
       for(int x = 0; x < 8; x++){
- 	if((x != 3 || x != 4) && (y != 3 || y != 4)){
+ 	if((x < 3 || x > 4) || (y < 3 || y > 4)){
 	  button[y][x] = new JButton(nocoin);
 	}else if((x == 3 && y == 3) || (x == 4 && y == 4)){
 	  button[y][x] = new JButton(black);
@@ -36,11 +43,55 @@ public class View implements MouseListener, MouseMotionListener {
 	  button[y][x] = new JButton(white);
 	}
         button[y][x].setBounds(x * 50, y * 50, 50, 50);
+	button[y][x].addMouseListener(this);
+	button[y][x].setActionCommand(Integer.toString(x) + "," + Integer.toString(y));
 	p.add(button[y][x]);
       }
     }
     frame.setVisible(true);
   }
 
-  public
+  public void update_button(int x, int y, int attribute){
+    if(Model.BLACK){
+      button[y][x] = new JButton(black);
+    }else if(Model.WHITE){
+      button[y][x] = new JButton(white);
+    }else if(Model.NO_COIN){
+      button[y][x] = new JButton(nocoin);
+    }
+  }
+
+  public void update_button(int board[][]){
+    for(int y = 0; y < 8; y++){
+      for(int x = 0; x < 8; x++){
+        update_button(y, x, board[y][x]);
+      }
+    }
+    repaint();
+  }
+
+  public int[] getClickedIndex(){
+    return clicked;
+  }
+
+  // mouse event method
+  private mouseClicked(MouseEvent e){
+    JButton clickedButton = (JButton)e.getComponent();
+    String indexOfTheBoard[] = clickedButton.getActionCommand().split(",", 0);
+    clicked[0] = indexOfTheBoard[0];
+    clicked[1] = indexOfTheBoard[1];
+  }
+
+  private mouseEntered(MouseEvent e){
+  }
+
+  private mouseExited(MouseEvent e){
+  }
+
+  private mousePressed(MouseEvent e){
+  }
+
+  private mouseReleased(MouseEvent e){
+  }
+
 }
